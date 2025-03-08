5df558687806f32ab96f1f0a26ea3c06a0eb326e
VovaSN <s.vova.dpo@gmail.com>
2025-03-08 20:37:35 +0400

    FixValidNumberFunc

--- Leetcode/RomanToInteger.java
+++ Leetcode/RomanToInteger.java
@@ -1,14 +1,9 @@
 public class RomanToInteger {
-    public static boolean isValidNumber(String s)
-    {
-        if(s.contains("IIII") || s.contains("VV") || s.contains("XXXX") || s.contains("LL") || s.contains("CCCC") || s.contains("DD")|| s.contains("MMMM"))
-        return false;
-        return true;
+    public static boolean isValidNumber(String s){
+        return s.contains("IIII") || s.contains("VV") || s.contains("XXXX") || s.contains("LL") || s.contains("CCCC") || s.contains("DD")|| s.contains("MMMM");
     }
-    public static int getNumber(char ch)
-    {
-        switch(ch)
-        {
+    public static int getNumber(char ch){
+        switch(ch){
             case 'I': return 1;
             case 'V': return 5;
             case 'X': return 10;
@@ -20,14 +15,13 @@ public class RomanToInteger {
         }
     }
     public static void main(String[] args) {
-        String s = "VI";
-        if(!isValidNumber(s)){
+        String s = "VV";
+        if(isValidNumber(s)){
             System.out.println("Error invalid Roman number");
         return;}
 
         int ans = 0;
-        for(int i = 0; i < s.length(); i++)
-        {
+        for(int i = 0; i < s.length(); i++){
             if(i + 1 < s.length() && getNumber(s.charAt(i)) < getNumber(s.charAt(i+1)))
             ans -= getNumber(s.charAt(i));
             else
