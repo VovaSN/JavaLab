dd956d8e54ad077595ba154a0fb66d2734c5482d
VovaSN <s.vova.dpo@gmail.com>
2025-03-08 20:35:38 +0400

    ValidParentheses
    Add ValidParentheses

---
+++ Leetcode/ValidParentheses.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+public class ValidParentheses {
+    private static boolean isPair(char curr, char last)
+    {
+        return (curr == '}' && last == '{') ||
+               (curr == ']' && last == '[') ||
+               (curr == ')' && last == '(');
+    }
+    public static boolean isValid(String s) {
+        Stack<Character> cs = new Stack<>();
+    for(int i = 0; i < s.length();i++){
+        char curr = s.charAt(i);
+        if(!cs.empty())
+        {
+            char last = cs.peek();
+            if(isPair(curr, last)){
+                    cs.pop();
+                    continue;
+                }
+
+        }
+        cs.push(curr);
+    }
+    return cs.isEmpty();
+
+}
+    public static void main(String[] args) {
+        String s = "[]";
+        System.out.println(isValid(s));
+}
+}
