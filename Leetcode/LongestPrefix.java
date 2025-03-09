99d93ab6857c5f78e16d92b8fdee6202bf46dce5
VovaSN <s.vova.dpo@gmail.com>
2025-03-09 12:22:15 +0400

    LongestCommonPrefix
    Add Problems: LongestCommonPrefix

---
+++ Leetcode/LongestPrefix.java
@@ -0,0 +1,22 @@
+public class LongestPrefix {
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+
+        String pref = strs[0];
+        int prefLen = pref.length();
+        
+        for(int i = 1; i < strs.length;i++){
+            while(prefLen > strs[i].length() || !pref.equals(strs[i].subSequence(0, prefLen))){
+            prefLen--;
+            if(prefLen == 0)
+            return "";
+            pref = pref.substring(0, prefLen);
+            } 
+        }return pref;
+    }
+    public static void main(String[] args) {
+        String strs[]= {"dog","racecar","car"};
+        System.out.println(longestCommonPrefix(strs));
+  
+    }
+}
